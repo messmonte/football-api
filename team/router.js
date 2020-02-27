2f2038ca4a549a4dc59eb03cad87417d12b2c1c2
messmonte <amandamariamonte@gmail.com>
2020-02-27 10:17:40 +0100

    Modified changes after redoing app

---
+++ team/router.js
@@ -0,0 +1,32 @@
+const { Router } = require("express");
+const Team = require("./model");
+
+const router = new Router();
+
+router.get("/team", (req, res, next) => {
+  Team.findAll()
+    .then(list => res.json(list))
+    .catch(next);
+});
+
+router.post("/team", (req, res, next) => {
+  Team.create(req.body)
+    .then(team => {
+      res.json(team);
+    })
+    .catch(next);
+});
+
+router.get("/team/:id", (req, res, next) => {
+  console.log("req.params.id :", typeof req.params.id);
+  Team.findByPk(req.params.id)
+    .then(team => res.json(team))
+    .catch(next);
+});
+
+router.get("/", (req, res, next) => {
+  console.log("Now I am showing something in the browser!");
+  res.send("Welcome to my backend api");
+});
+
+module.exports = router;
